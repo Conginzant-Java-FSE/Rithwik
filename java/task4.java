304c366e6f9c474aa7e3d82d6eba5b26f4be2387
rithwik1473 <rithwik.akkana@gmail.com>
2026-01-12 15:29:07 +0530

    code passed

---
+++ java/task4.java
@@ -0,0 +1,79 @@
+abstract class Wizard{
+    String name;
+    int powerLevel;
+    public Wizard(String name, int powerLevel){
+        this.name = name;
+        this.powerLevel = powerLevel;
+    }
+    abstract int castSpell(String spellName);
+    public String getName() {
+        return name;
+    }
+}
+class DarkWizard extends Wizard {
+    public DarkWizard(String name, int powerLevel) {
+        super(name,powerLevel);
+    }
+    @Override
+    int castSpell(String spellName) {
+        switch (spellName) {
+            case "Crucio":
+                return powerLevel * 3;
+            case "AvadaKedavra":
+                return powerLevel * 5;
+            default:
+                return powerLevel;
+        }
+    }
+}
+class HealingWizard extends Wizard {
+    public HealingWizard(String name, int powerLevel) {
+        super(name, powerLevel);
+    }
+    @Override
+    int castSpell(String spellName) {
+        switch (spellName) {
+            case "Heal":
+                return powerLevel * 2;
+            case "Revive":
+                return powerLevel * 4;
+            default:
+                return 0;
+        }
+    }
+}
+
+class ElementWizard extends Wizard {
+    public ElementWizard(String name, int powerLevel) {
+        super(name, powerLevel);
+    }
+
+    @Override
+    int castSpell(String spellName) {
+        switch (spellName) {
+            case "Fireball":
+                return powerLevel * 2;
+            case "Lightning":
+                return powerLevel * 3;
+            default:
+                return powerLevel;
+        }
+    }
+}
+
+
+public class task4 {
+
+    public static void main(String args[]){
+
+        Wizard wizard = new DarkWizard("Voldemort", 80);
+
+        String spell = "AvadaKedavra";
+        int damage = wizard.castSpell(spell);
+
+        System.out.println("Wizard: " + wizard.getName());
+        System.out.println("Spell Casted: " + spell);
+        System.out.println("Damage Dealt: " + damage);
+    }
+
+}
---
+++ java/task5.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+abstract class Missile {
+    String missileId;
+    int range;
+
+    Missile(String missileId, int range) {
+        this.missileId = missileId;
+        this.range = range;
+    }
+
+    abstract boolean launch(int threatLevel, boolean hasClearance);
+}
+
+class NuclearMissile extends Missile {
+    NuclearMissile(String missileId, int range) {
+        super(missileId, range);
+    }
+
+    boolean launch(int threatLevel, boolean hasClearance) {
+        System.out.println("Missile ID: " + missileId);
+        if (threatLevel >= 9 && hasClearance) {
+            System.out.println("Launch Status: APPROVED");
+            System.out.println("Nuclear missile launched successfully.");
+            return true;
+        } else {
+            System.out.println("Launch Status: DENIED");
+            if (threatLevel < 9) {
+                System.out.println("Reason: Threat level insufficient for nuclear launch");
+            } else {
+                System.out.println("Reason: Command clearance not provided");
+            }
+            return false;
+        }
+    }
+}
+
+class TorpedoMissile extends Missile {
+    TorpedoMissile(String missileId, int range) {
+        super(missileId, range);
+    }
+
+    boolean launch(int threatLevel, boolean hasClearance) {
+        System.out.println("Missile ID: " + missileId);
+        if (threatLevel >= 5) {
+            System.out.println("Launch Status: APPROVED");
+            System.out.println("Torpedo launched successfully.");
+            return true;
+        } else {
+            System.out.println("Launch Status: DENIED");
+            System.out.println("Reason: Threat level insufficient for torpedo launch");
+            return false;
+        }
+    }
+}
+
+class DefenseInterceptor extends Missile {
+    DefenseInterceptor(String missileId, int range) {
+        super(missileId, range);
+    }
+
+    boolean launch(int threatLevel, boolean hasClearance) {
+        System.out.println("Missile ID: " + missileId);
+        if (threatLevel >= 3) {
+            System.out.println("Launch Status: APPROVED");
+            System.out.println("Interceptor launched successfully.");
+            return true;
+        } else {
+            System.out.println("Launch Status: DENIED");
+            System.out.println("Reason: Threat level insufficient for interceptor launch");
+            return false;
+        }
+    }
+}
+
+public class task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Missile Type: ");
+        String missileType = scanner.nextLine();
+
+        System.out.print("Missile ID: ");
+        String missileId = scanner.nextLine();
+
+        System.out.print("Threat Level: ");
+        int threatLevel = scanner.nextInt();
+
+        System.out.print("Clearance (true/false): ");
+        boolean hasClearance = scanner.nextBoolean();
+
+        Missile missile = null;
+
+        switch (missileType) {
+            case "NuclearMissile":
+                missile = new NuclearMissile(missileId, 5000);
+                break;
+            case "TorpedoMissile":
+                missile = new TorpedoMissile(missileId, 100);
+                break;
+            case "DefenseInterceptor":
+                missile = new DefenseInterceptor(missileId, 200);
+                break;
+            default:
+                System.out.println("Invalid missile type");
+                scanner.close();
+                return;
+        }
+
+        System.out.println();
+        missile.launch(threatLevel, hasClearance);
+        scanner.close();
+    }
+}
